ab9f1aed9513a64f3f73e0f99e6d573e3af2d39e
Lucca Giordano <giordanolucca871@gmail.com>
2023-03-10 18:47:45 -0300

    initial files

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+/*
+
+const downloadVideo = async (videoUrl, filename) => {
+    const response = await fetch(videoUrl)
+    const blob = await response.blob()
+    const link = document.createElement('a')
+    link.href = URL.createObjectURL(blob)
+    link.download = filename
+    link.click()
+}
+
+*/
