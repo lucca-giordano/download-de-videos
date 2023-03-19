2ecbe33ef184ad9d49ddd4ee3553ba731f4b3401
Lucca Giordano <giordanolucca871@gmail.com>
2023-03-18 21:38:58 -0300

    downloadjs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
-const axios = require('axios');
-const fs = require('fs');
+require("downloadjs")(data, strFileName, strMimeType);
+
+import axios, { get } from 'axios';
+import download from 'downloadjs';
+import { createWriteStream } from 'fs';
 
 const tweetUrl = document.querySelector('.textinput').value;
 
 const downloadButton = document.querySelector('.button');
 
 downloadButton.addEventListener('click', () => {
-    axios.get(tweetUrl)
+    /*
+    get(tweetUrl)
     .then(response => {
     const html = response.data;
     const videoUrl = html.match(/"content_url":"(.*?)"/)[1];
@@ -18,9 +22,13 @@ downloadButton.addEventListener('click', () => {
         }
     )
     .then(response => {
-        response.data.pipe(fs.createWriteStream('myvideo.mp4'));
+        response.data.pipe(createWriteStream('myvideo.mp4'));
             }
         );
     }
 );
+*/
+
+download(tweetUrl, 'myvideo.mp4', 'video/mp4')
+
 });
